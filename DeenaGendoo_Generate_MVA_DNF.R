d46b40332ee538c1dfacb1be561607dc1067d3a3
ChrisLam <fdq09eca@gmail.com>
2021-07-02 23:57:36 +0100

    pass drug_sanity_check

--- DeenaGendoo_Generate_MVA_DNF.R
+++ DeenaGendoo_Generate_MVA_DNF.R
@@ -16,30 +16,49 @@ gmt_path <- "Data/GMT/c2.cp.kegg.v7.4.symbols.gmt"
 gmt <- read_gmt(gmt_path = gmt_path, logger = DNF_logger)
 
 i <- 1
-pathway <- as.character(sort(gmt$genesets[[i]]))
-mva <- read.csv("Data/MVA_genes.csv")
-mvagenes <- as.character(sort(mva$Gene.name))
-num_gene_match_with_pertData <- length(which(rownames(pertData) %in% pathway))
+pathway_name <- gmt$geneset.names[i]
+pathway_genes <- as.character(sort(gmt$genesets[[i]]))
+# mva <- read.csv("Data/MVA_genes.csv")
+# mvagenes <- as.character(sort(mva$Gene.name))
 
-pertData <- pertData[(which(rownames(pertData) %in% mva$Gene.name)), ]
-dim(pertData)
-rownames(pertData)
+pert_genes <- rownames(pertData)
+common_genes <- Reduce(intersect, list(pert_genes, pathway_genes))
+num_common_genes <- length(common_genes)
+pertData <- pertData[common_genes, ]
+
+log4r::info(DNF_logger, paste(
+    "{{", num_common_genes, "}} common genes between pertData and pathway {{",
+    pathway_name, "}}: {{", common_genes, "}}"
+))
+# dim(pertData)
+# rownames(pertData)
 
 # Reduce All Matrices to lowest common set of drugs
 # Get 237 drugs now in the reduced sets
-commonDrugs <- Reduce(intersect, list(
-    sort(names(strcData)), sort(colnames(sensData)),
-    sort(colnames(pertData))
-))
+
+pert_drugs <- if (is(pertData, "matrix")) sort(colnames(pertData)) else sort(names(pertData))
+strc_drugs <- names(strcData)
+senes_drugs <- sort(colnames(sensData))
+
+commonDrugs <- Reduce(intersect, list(pert_drugs, strc_drugs, senes_drugs))
+
+pertData <- if (is(pertData, "matrix")) pertData[, commonDrugs] else pertData[commonDrugs]
 strcData <- strcData[commonDrugs]
 sensData <- sensData[, commonDrugs]
-pertData <- pertData[, commonDrugs]
 
 # Sanity Checks
-if (ncol(sensData) != ncol(pertData)) stop(sprintf("error!"))
-if (ncol(sensData) != length(strcData)) stop(sprintf("error!"))
-if (all(colnames(pertData) != colnames(sensData))) stop(sprintf("error!"))
-if (all(colnames(pertData) != names(strcData))) stop(sprintf("error!"))
+drug_sanity_check(pertData, sensData, strcData, DNF_logger)
+# if (num_common_genes == 1) {
+#     if (ncol(sensData) != length(pertData)) stop(sprintf("error!"))
+#     if (all(names(pertData) != colnames(sensData))) stop(sprintf("error!"))
+#     if (all(names(pertData) != names(strcData))) stop(sprintf("error!"))
+# } else {
+#     if (ncol(sensData) != ncol(pertData)) stop(sprintf("error!"))
+#     if (all(colnames(pertData) != colnames(sensData))) stop(sprintf("error!"))
+#     if (all(colnames(pertData) != names(strcData))) stop(sprintf("error!"))
+# }
+# if (ncol(sensData) != length(strcData)) stop(sprintf("error!"))
+
 
 ## network layer construction and integration by SNF
 strcAffMat <- constStructureLayer(strcData)
