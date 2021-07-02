bce76e1eb314a3214555c15f78d44cc0d4a218fe
ChrisLam <fdq09eca@gmail.com>
2021-07-02 10:19:58 +0100

    fmt

--- DeenaGendoo_PermutationTestAndFiltering.R
+++ DeenaGendoo_PermutationTestAndFiltering.R
@@ -3,13 +3,9 @@
 # Calculate the significance of getting a particular drug ranking in MVA-DNF
 ################################################################################
 ################################################################################
-Pathway="MVA"
-
-source('FunctionsBank.R')
-
-library(PharmacoGx) 
+library(PharmacoGx)
 library(apcluster)
-library(rcdk)
+# library(rcdk)
 library(fingerprint)
 library(annotate)
 library(org.Hs.eg.db)
@@ -19,93 +15,99 @@ library(survcomp)
 library(reshape2)
 library(proxy)
 library(pheatmap)
+source("FunctionsBank.R")
 
-InterestDrug<-"DIPYRIDAMOLE" #dipyridamole,fluvastatin,methotrexate
-NumHits<-238 #Number of top hits to tabulate agains the query drug. Use all drugs here
+Pathway <- "MVA"
+InterestDrug <- "DIPYRIDAMOLE" # dipyridamole,fluvastatin,methotrexate
+NumHits <- 238 # Number of top hits to tabulate agains the query drug. Use all drugs here
 
 badchars <- "[\xb5]|[\n]|[,]|[;]|[:]|[-]|[+]|[*]|[%]|[$]|[#]|[{]|[}]|[[]|[]]|[|]|[\\^]|[/]|[\\]|[.]|[_]|[ ]"
 
-Iterations<-999
-DrugHitMatrix<-matrix(data = NA,nrow = 238,ncol = 1)
+Iterations <- 999
+DrugHitMatrix <- matrix(data = NA, nrow = 238, ncol = 1)
 
 ################################################
+
 load("Data/MVA_DNF.RData")
-W<-integrtStrctSensPert
-InterestDrug.SNF<-W[InterestDrug,]
-DP_Hits_MVA<-names(sort(InterestDrug.SNF,decreasing = T))
+W <- integrtStrctSensPert
+InterestDrug.SNF <- W[InterestDrug, ]
+DP_Hits_MVA <- names(sort(InterestDrug.SNF, decreasing = T))
 
 set.seed(12345)
 
-#Load the Permutation Matrix Used in the publication
-load("Data/DrugHitMatrix.RData") 
-if(!exists("DrugHitMatrix"))
-{
-  #Iterative repeat
-  for(count in 1:Iterations)
+# Load the Permutation Matrix Used in the publication
+load("Data/DrugHitMatrix.RData")
+if (!exists("DrugHitMatrix")) {
+  # Iterative repeat
+  for (count in 1:Iterations)
   {
-    message("Iteration: ",count)
-  
-    load("Data/PrePros.RData") #loading data layers
-    
-    RandomGenes<-sample(rownames(pertData),6)
-    pertData<-pertData[(which(rownames(pertData) %in% RandomGenes)),] 
-  
-    commonDrugs <- Reduce(intersect,list(sort(names(strcData)),sort(colnames(sensData)),
-                                         sort(colnames(pertData))))
-    strcData<- strcData[commonDrugs]
-    sensData <- sensData[,commonDrugs]
-    pertData <- pertData[,commonDrugs]
-    
+    message("Iteration: ", count)
+
+    load("Data/PrePros.RData") # loading data layers
+
+    RandomGenes <- sample(rownames(pertData), 6) # there are 6 gene in the mva_gene.csv
+    pertData <- pertData[(which(rownames(pertData) %in% RandomGenes)), ]
+
+    commonDrugs <- Reduce(intersect, list(
+      sort(names(strcData)), sort(colnames(sensData)),
+      sort(colnames(pertData))
+    ))
+    strcData <- strcData[commonDrugs]
+    sensData <- sensData[, commonDrugs]
+    pertData <- pertData[, commonDrugs]
+
     strcAffMat <- constStructureLayer(strcData)
     sensAffMat <- constSensitivityLayer(sensData)
     pertAffMat <- constPerturbationLayer(pertData)
+    
     integrtStrctSensPert <- integrateStrctSensPert(sensAffMat, strcAffMat, pertAffMat)
     
-    InterestDrug.SNF<-integrtStrctSensPert[InterestDrug,]
-    TOPS<-names(sort(InterestDrug.SNF,decreasing = T))
-    
-    DrugHitMatrix<-cbind(DrugHitMatrix,TOPS)
+    InterestDrug.SNF <- integrtStrctSensPert[InterestDrug, ]
+    TOPS <- names(sort(InterestDrug.SNF, decreasing = T))
+
+    DrugHitMatrix <- cbind(DrugHitMatrix, TOPS)
   }
 }
 
-DrugHitMatrix<-DrugHitMatrix[,-1]
-colnames(DrugHitMatrix)<-c(paste("Round_",rep(1:ncol(DrugHitMatrix)),sep = ""))
+DrugHitMatrix <- DrugHitMatrix[, -1]
+colnames(DrugHitMatrix) <- c(paste("Round_", rep(1:ncol(DrugHitMatrix)), sep = ""))
 
-DrugHitMatrix<-cbind("MVA"=DP_Hits_MVA,DrugHitMatrix)
+DrugHitMatrix <- cbind("MVA" = DP_Hits_MVA, DrugHitMatrix)
 
-save(DrugHitMatrix,file="DrugHitMatrix.RData")
+save(DrugHitMatrix, file = "DrugHitMatrix.RData")
 
-DrugList<-DrugHitMatrix[,1]
+DrugList <- DrugHitMatrix[, 1]
 
-Matrix_RANK<-data.frame(DrugList)  
-for(sample in 1:ncol(DrugHitMatrix))
+Matrix_RANK <- data.frame(DrugList)
+
+for (sample in 1:ncol(DrugHitMatrix))
 {
-  TempRankHuman<-DrugHitMatrix[,sample]
-  TempRankHuman<-TempRankHuman[which(TempRankHuman %in% DrugList)]
-  Matrix_RANK[,(colnames(DrugHitMatrix)[sample])]<-match(factor(DrugList),factor(TempRankHuman))
+  TempRankHuman <- DrugHitMatrix[, sample]
+  TempRankHuman <- TempRankHuman[which(TempRankHuman %in% DrugList)]
+  Matrix_RANK[, (colnames(DrugHitMatrix)[sample])] <- match(factor(DrugList), factor(TempRankHuman))
 }
-rownames(Matrix_RANK)<-DrugList
-Matrix_RANK<-Matrix_RANK[,-1]
+rownames(Matrix_RANK) <- DrugList
+Matrix_RANK <- Matrix_RANK[, -1]
 
 
-RanksZScore<-RanksPVal<-RanksPVal2<-NULL
+RanksZScore <- RanksPVal <- RanksPVal2 <- NULL
 
-for(counter in 1:nrow(Matrix_RANK))
+for (counter in 1:nrow(Matrix_RANK))
 {
-  DrugRanksForSelectedDrug<-as.numeric(Matrix_RANK[counter,])
-  
-  pop_sd<-sd(DrugRanksForSelectedDrug)
+  DrugRanksForSelectedDrug <- as.numeric(Matrix_RANK[counter, ])
+
+  pop_sd <- sd(DrugRanksForSelectedDrug)
   pop_mean <- mean(DrugRanksForSelectedDrug)
   zscore <- (counter - pop_mean) / pop_sd
   pvalue <- pnorm(counter, pop_mean, pop_sd)
 
-  RanksZScore<-c(RanksZScore,zscore)
-  RanksPVal<-c(RanksPVal,pvalue)
+  RanksZScore <- c(RanksZScore, zscore)
+  RanksPVal <- c(RanksPVal, pvalue)
 }
 
-FullMatrix<-(cbind("MVA_Rank"=Matrix_RANK[,1],RanksZScore,RanksPVal))
-rownames(FullMatrix)<-rownames(Matrix_RANK)
+FullMatrix <- (cbind("MVA_Rank" = Matrix_RANK[, 1], RanksZScore, RanksPVal))
+rownames(FullMatrix) <- rownames(Matrix_RANK)
 
-View(FullMatrix[FullMatrix[,3]<0.05,])
+View(FullMatrix[FullMatrix[, 3] < 0.05, ])
 
-write.csv(FullMatrix,file="DrugHit_StatScores.csv")
+write.csv(FullMatrix, file = "DrugHit_StatScores.csv")
