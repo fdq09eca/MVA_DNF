fc708427f5a7f77acbd4a06c542058ee810c8066
ChrisLam <fdq09eca@gmail.com>
2021-07-02 10:19:39 +0100

    auto install bioc packages, fmt

--- DeenaGendoo_Generate_MVA_DNF.R
+++ DeenaGendoo_Generate_MVA_DNF.R
@@ -3,9 +3,31 @@
 # Generat3 the MVA-DNF matrix, using set of MVA-defined genes (genes identified by Dr. Linda Penn's lab)
 ########################################################################
 ########################################################################
-library(PharmacoGx) 
+load_bio_package <- function(pkg) {
+    if (!requireNamespace("BiocManager", quietly = TRUE)) {
+          install.packages("BiocManager")
+      }
+    if (!require(pkg, character.only = TRUE)) {
+          BiocManager::install(pkg)
+      }
+    if (!require(pkg, character.only = TRUE)) {
+          stop(paste("Failed to install package: ", pkg))
+      }
+}
+
+bioconductor_packages <- c("PharmacoGx", "annotate", "org.Hs.eg.db", "survcomp")
+carn_packages <- c(
+    "apcluster", "rcdk", "fingerprint",
+    "SNFtool", "ROCR", "reshape2", "proxy", "pheatmap"
+)
+
+
+for (pkg in bioconductor_packages) load_bio_package(pkg)
+install.packages(setdiff(carn_packages, rownames(installed.packages())))
+
+library(PharmacoGx)
 library(apcluster)
-library(rcdk)
+# library(rcdk)
 library(fingerprint)
 library(annotate)
 library(org.Hs.eg.db)
@@ -17,26 +39,28 @@ library(proxy)
 
 badchars <- "[\xb5]|[\n]|[,]|[;]|[:]|[-]|[+]|[*]|[%]|[$]|[#]|[{]|[}]|[[]|[]]|[|]|[\\^]|[/]|[\\]|[.]|[_]|[ ]"
 
-mva<-read.csv("Data/MVA_genes.csv")
-mvagenes<-as.character(sort(mva$Gene.name))
+mva <- read.csv("Data/MVA_genes.csv")
+mvagenes <- as.character(sort(mva$Gene.name))
 
-source('FunctionsBank.R')
+source("FunctionsBank.R")
 load("Data/PrePros.RData")
-pertData<-pertData[(which(rownames(pertData) %in% mva$Gene.name)),] 
+pertData <- pertData[(which(rownames(pertData) %in% mva$Gene.name)), ]
 dim(pertData)
 rownames(pertData)
 
-# Reduce All Matrices to lowest common set of drugs 
+# Reduce All Matrices to lowest common set of drugs
 # Get 237 drugs now in the reduced sets
-commonDrugs <- Reduce(intersect,list(sort(names(strcData)),sort(colnames(sensData)),
-                                     sort(colnames(pertData))))
-strcData<- strcData[commonDrugs]
-sensData <- sensData[,commonDrugs]
-pertData <- pertData[,commonDrugs]
+commonDrugs <- Reduce(intersect, list(
+    sort(names(strcData)), sort(colnames(sensData)),
+    sort(colnames(pertData))
+))
+strcData <- strcData[commonDrugs]
+sensData <- sensData[, commonDrugs]
+pertData <- pertData[, commonDrugs]
 
-#Sanity Checks
+# Sanity Checks
 if (ncol(sensData) != ncol(pertData)) stop(sprintf("error!"))
-if (ncol(sensData) !=length(strcData)) stop(sprintf("error!"))
+if (ncol(sensData) != length(strcData)) stop(sprintf("error!"))
 if (all(colnames(pertData) != colnames(sensData))) stop(sprintf("error!"))
 if (all(colnames(pertData) != names(strcData))) stop(sprintf("error!"))
 
@@ -53,11 +77,4 @@ dim(pertAffMat)
 dim(integrtStrctSensPert)
 
 # Save an RData Object with all matrices: MVA-DNF and single layer taxonomies
-save(integrtStrctSensPert,strcAffMat,sensAffMat,pertAffMat, file="MVA_DNF.RData")
-
-
-
-
-
-
-
+save(integrtStrctSensPert, strcAffMat, sensAffMat, pertAffMat, file = "MVA_DNF.RData")
